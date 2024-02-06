e654d0bd73bde150ddb798810beb32d75bc68f89
Bayasaa <bayarbayasgalan.j@gmail.com>
2024-02-05 21:38:19 -0600

    dsd

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import random as random
+
 print (random.randrange(1, 100))
-
-
-
+print (random.randrange(1, 100))
+print (random.randrange(1, 100))
+print (random.randrange(1, 100))
+print (random.randrange(1, 100))
+print (random.randrange(1, 200))
+print (random.randrange(1, 100))
 
 print ("dddd")
